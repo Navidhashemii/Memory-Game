22c8be69400678d4a07fe46b6e03f04919389fbf
Navid Hashemi <nhashemi007@yahoo.com>
2023-09-25 12:24:40 +0330

    add card images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 
 import './App.css';
 
+const cardImages = [
+  {"src" : "/images/bear.png"},
+  {"src" : "/images/dog.png"},
+  {"src" : "/images/eagle.png"},
+  {"src" : "/images/elephant.png"},
+  {"src" : "/images/horse.png"},
+  {"src" : "/images/lion.png"},
+  {"src" : "/images/pig.png"},
+  {"src" : "/images/rabbit.png"},
+  {"src" : "/images/Tiger.png"},
+  {"src" : "/images/zebra.png"}
+]
+
 function App() {
   return (
     <div className="App">
